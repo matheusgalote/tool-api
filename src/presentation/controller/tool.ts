ec6a54389da9e770eb877d715ae6a59239ee6959
matheusgalote <mateugalote@gmail.com>
2023-08-20 11:37:57 -0300

    refactor: improved required field validation

--- src/presentation/controller/tool.ts
+++ src/presentation/controller/tool.ts
@@ -4,11 +4,11 @@ import type { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class ToolController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.name) {
-      return missingParamError('name')
-    }
-    if (!httpRequest.body.code) {
-      return missingParamError('code')
+    const requiredFields = ['name', 'code']
+    for (const requiredField of requiredFields) {
+      if (!httpRequest.body[requiredField]) {
+        return missingParamError(requiredField)
+      }
     }
     return null
   }
