3398a22fc3380e9eaf30a59c0787230e56aa692d
matheusgalote <mateugalote@gmail.com>
2023-08-20 11:51:03 -0300

    test: ensure ToolController return success if all required data is provided in request

--- src/presentation/controller/tool.spec.ts
+++ src/presentation/controller/tool.spec.ts
@@ -1,6 +1,16 @@
 import { ToolController } from './tool'
 import type { HttpRequest } from '../protocols/http'
-import { missingParamError } from '../helpers/http/http-helper'
+import { missingParamError, ok } from '../helpers/http/http-helper'
+
+function makeFakeHttpRequest (): HttpRequest {
+  return {
+    body: {
+      name: 'any_name',
+      code: 'any_code',
+      description: 'any_description'
+    }
+  }
+}
 
 describe('Tool Controller', () => {
   test('Should return error if validation name is not provided', async () => {
@@ -29,5 +39,11 @@ describe('Tool Controller', () => {
     const { body, statusCode } = missingParamError('code')
     expect(error.body).toEqual(body)
     expect(error.statusCode).toBe(statusCode)
+  })
+
+  test('Should return ok if all data is provided', async () => {
+    const sut = new ToolController()
+    const success = await sut.handle(makeFakeHttpRequest())
+    expect(success).toEqual(ok(makeFakeHttpRequest()))
   })
 })
