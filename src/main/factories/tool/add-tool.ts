7e9cb3ea9c9550f70c3db005d9f2f1082fb5ff01
matheusgalote <mateugalote@gmail.com>
2023-08-22 21:13:10 -0300

    feat: ensure ToolController calls persistence class

--- src/main/factories/tool/add-tool.ts
+++ src/main/factories/tool/add-tool.ts
@@ -1,6 +1,11 @@
+import { DbAddTool } from '../../../data/usecases/add-tool/db-add-tool'
+import { ToolMongoRepository } from '../../../infra/db/mongodb/tool-repository/tool'
 import { AddToolController } from '../../../presentation/controller/tool/add-tool'
 import type { Controller } from '../../../presentation/protocols/contoller'
 
 export const makeAddToolController = (): Controller => {
-  return new AddToolController()
+  const toolMongoRepository = new ToolMongoRepository()
+  const dbAddTool = new DbAddTool(toolMongoRepository)
+
+  return new AddToolController(dbAddTool)
 }
